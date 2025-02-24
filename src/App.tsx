5c77080cb9c7f9f8aeef0f9a6dc87af54d3d4caa
Rohandede1825 <rohandede97@gmail.com>
2025-02-24 15:12:28 +0530

    setup the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='bg-cyan-500 text-white p-4'>
+        hi there
+      </div>
+    </>
+  )
+}
+
+export default App
